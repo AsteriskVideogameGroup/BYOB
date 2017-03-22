c8a6c8f022d5115778b5dd9d278c23e574f04855
castogio <gioacchino.castorio@icloud.com>
2017-03-22 20:02:21 +0100

    Aggiunti GameModeFactory e IGameModeFactory

---
+++ src/model/GameModeFactory.py
@@ -0,0 +1,22 @@
+class GameModeFactory:
+    class __Implementation:
+        def __init__(self):
+            pass
+
+        def getGameMode() -> :
+            """ Request to join a new Game """
+
+            # TODO
+            pass
+
+    _instance: __Implementation = None  # singleton instance
+
+    def __init__(self):
+        raise SyntaxError  # throw exception
+
+    @classmethod
+    def getInstance(cls) -> __Implementation:
+        if not cls._instance:
+            cls._instance = cls.__Implementation()
+
+        return cls._instance
---
+++ src/model/IGameMode.py
@@ -0,0 +1,2 @@
+class IGameMode:
+    pass
