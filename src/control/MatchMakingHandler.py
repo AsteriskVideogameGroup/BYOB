29d34f89cd1c27a61b0e5f10f5db38a6e5e5b3c6
castogio <gioacchino.castorio@icloud.com>
2017-03-22 17:40:10 +0100

    Initial Commit

---
+++ src/control/MatchMakingHandler.py
@@ -0,0 +1,10 @@
+class MatchMakingHandler:
+    _instance = None  # singleton instance
+
+    def __init__(self):
+        print "Hello world"
+
+
+    class SingleInstance:
+        def __init__(self):
+            pass
