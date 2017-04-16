3ee38093c2396bf9b4f548e412b056467e625571
Angelo <angelo.damiani.ch@gmail.com>
2017-04-16 18:21:21 +0200

    .

--- src/main_angelo.py
+++ src/main_angelo.py
@@ -1,3 +1,8 @@
+import sys
+import os.path
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 import src.control.gamemanage.MatchMakingHandler as MatchMakingHandler
 import src.control.gamemanage.GameHandler as GameHandler
 import src.domain.gamemanage.player.Player as Player
